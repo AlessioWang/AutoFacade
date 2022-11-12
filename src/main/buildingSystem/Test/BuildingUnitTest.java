0654083ef59d974a0aed55f278016322026e57d0
Alessio <1013797789@qq.com>
2022-11-12 18:28:50 +0800

    Some test changes

--- src/main/buildingSystem/Test/BuildingUnitTest.java
+++ src/main/buildingSystem/Test/BuildingUnitTest.java
@@ -6,11 +6,9 @@ import org.junit.Test;
 import processing.core.PApplet;
 import unit2Vol.Unit;
 import unit2Vol.UnitRender;
-import unit2Vol.face.Face;
 import wblut.geom.WB_Point;
 import wblut.geom.WB_Polygon;
 import wblut.geom.WB_Vector;
-import wblut.processing.WB_Render;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -44,15 +42,27 @@ public class BuildingUnitTest extends PApplet {
         WB_Polygon base = GeoTools.createRecPolygon(12000, 8000);
         WB_Vector dir = new WB_Vector(2, 1, 0);
 
-        double gap = 12000;
+
+        double gap = 8000;
+        double height = 3500;
 
         units = new LinkedList<>();
 
-        for (int i = 0; i < 10; i++) {
-            WB_Point p = pos.add(new WB_Point(1, 0, 0).mul(gap).mul(i));
-            Unit u = new Unit(p, base, dir, 3500);
-            units.add(u);
-            unitRenders.add(new UnitRender(this, u));
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                WB_Vector increase = dir.div(dir.normalizeSelf());
+
+                WB_Vector horDirNor = pos.add(new WB_Point(1, 0, 0).mul(gap).mul(i));
+                double length = horDirNor.dot(dir);
+                WB_Vector v = increase.mul(length);
+
+                WB_Point p = pos.add(v
+                        .add(new WB_Point(0, 0, 1).mul(height).mul(j)));
+
+                Unit u = new Unit(p, base, dir, height);
+                units.add(u);
+                unitRenders.add(new UnitRender(this, u));
+            }
         }
 
     }
@@ -64,7 +74,6 @@ public class BuildingUnitTest extends PApplet {
         for (UnitRender ur : unitRenders) {
             ur.renderAll();
         }
-
     }
 
 }
