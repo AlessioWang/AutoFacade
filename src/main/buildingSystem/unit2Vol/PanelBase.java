1fa81b398ab68ecfd5b8c9cc627a11fff2ffa9fb
Alessio <1013797789@qq.com>
2022-12-12 18:10:45 +0800

    add class: PanelBase.java

---
+++ src/main/buildingSystem/unit2Vol/PanelBase.java
@@ -0,0 +1,12 @@
+package unit2Vol;
+
+/**
+ * 由face中的基本几何信息组合或者拆分的Class
+ * 解决的是会议等大空间组合的立面关系，或者一个教室对应的立面由若干种panel组成的场景
+ *
+ * @auther Alessio
+ * @date 2022/12/12
+ **/
+public class PanelBase {
+
+}
